2ac3fc5375b309a3fbe63925db8e6f749832a005
y667li <y667li@student.cs.uwaterloo.ca>
2016-10-12 00:21:29 -0400

    clean up code, rename exit_cv to iteration_cv

--- os161-1.99/kern/synchprobs/traffic_synch.c
+++ os161-1.99/kern/synchprobs/traffic_synch.c
@@ -4,16 +4,9 @@
 #include <synch.h>
 #include <opt-A1.h>
 
-/* 
- * Replace this default synchronization mechanism with your own (better) mechanism
- * needed for your solution.   Your mechanism may use any of the available synchronzation
- * primitives, e.g., semaphores, locks, condition variables.   You are also free to 
- * declare other global variables if your solution requires them.
- */
-
-
 /*
-My Solution:
+
+Solution:
 
 The condition for a car Va to pass is, for all Vb all followings
 are true:
@@ -26,20 +19,31 @@ For (1) (2) we use 4 integers representing # of cars from origins,
 and 4 integers represeting the same for dests
 For (3) we also a integer representing # of cars NOT making a right turn
 
+We also trace how many cars are passing, waiting, and exiting the current
+iteration.
+
+Effiency:
+
+For each iteration, we make each car keeps trying and passing at the first
+chance they get.
+
+We make sure that each thread should wait for at most one service time per other
+thread in the system. To do this we use a iteration_cv which puts the current
+iteration threads to sleep after exiting the intersection, and wake them up
+after all cars in the current iteration have passed, to start the second
+iteration.
+
 */
 
-/*
- * replace this with declarations of any synchronization and other variables you need here
- */
-volatile unsigned int origin[4] = {0};
-volatile unsigned int destination[4] = {0};
-volatile unsigned int num_not_right = 0;
-volatile unsigned int passing = 0;
-volatile unsigned int waiting = 0;
-volatile unsigned int exiting = 0;
-struct lock* intersection_lock;
-struct cv* intersection_cv;
-struct cv* exit_cv;
+static volatile unsigned int origin[4] = {0};
+static volatile unsigned int destination[4] = {0};
+static volatile unsigned int num_not_right = 0;
+static volatile unsigned int passing = 0;
+static volatile unsigned int waiting = 0;
+static volatile unsigned int exiting = 0;
+static struct lock* intersection_lock;
+static struct cv* intersection_cv;
+static struct cv* iteration_cv;
 
 /* helpers */
 
@@ -108,7 +112,7 @@ void
 intersection_sync_init(void)
 {
   intersection_cv = cv_create("Intersection CV");
-  exit_cv = cv_create("Intersection Prepare CV");
+  iteration_cv = cv_create("Iteration CV");
   intersection_lock = lock_create("Intersection Lock");
 }
 
@@ -123,7 +127,7 @@ void
 intersection_sync_cleanup(void)
 {
   cv_destroy(intersection_cv);
-  cv_destroy(exit_cv);
+  cv_destroy(iteration_cv);
   lock_destroy(intersection_lock);  
 }
 
@@ -145,16 +149,17 @@ void
 intersection_before_entry(Direction origin, Direction destination) 
 {
   lock_acquire(intersection_lock);
+  // wait until the car can pass
   while (! car_can_pass(origin, destination)) {
+    // try next car of the same iteration
     cv_signal(intersection_cv, intersection_lock);
-    cv_signal(exit_cv, intersection_lock);
     waiting++;
     cv_wait(intersection_cv, intersection_lock);
     waiting--;
   }
   car_passing(origin, destination);
+  // try next car of the same iteration
   cv_signal(intersection_cv, intersection_lock);
-  cv_signal(exit_cv, intersection_lock);
   lock_release(intersection_lock);
 }
 
@@ -175,17 +180,19 @@ intersection_after_exit(Direction origin, Direction destination)
 {
   lock_acquire(intersection_lock);
   car_passed(origin, destination);
-  cv_signal(intersection_cv, intersection_lock);
-  while (waiting + passing == 0 && exiting > 0) {
-    lock_release(intersection_lock);
-    cv_signal(exit_cv, intersection_lock);
-    lock_acquire(intersection_lock);
+  // when there're cars in the current iteration waiting,
+  // ask the next cars to pass
+  if (waiting > 0) {
+    cv_signal(intersection_cv, intersection_lock);
   }
-  while (waiting + passing > 1) {
+  // if the current iteration is still running, 
+  // wait until the current iteration finishes
+  while (waiting + passing > 0) {
     exiting++;
-    cv_wait(exit_cv, intersection_lock);
+    cv_wait(iteration_cv, intersection_lock);
     exiting--;
   } 
-  kprintf("passing=%d, waiting=%d, exiting=%d\n", passing, waiting, exiting);
+  // the current iteration is clear, wake up next iteration
+  cv_signal(iteration_cv, intersection_lock);
   lock_release(intersection_lock);
 }
