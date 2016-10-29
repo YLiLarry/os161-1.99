f7aa87bf3ca06a8b88dd50c54dd30831f51e364e
y667li <y667li@student.cs.uwaterloo.ca>
2016-10-29 15:42:55 -0400

    spacechange

--- os161-1.99/kern/proc/proc.c
+++ os161-1.99/kern/proc/proc.c
@@ -1,6 +1,6 @@
 /*
  * Copyright (c) 2013
- *	The President and Fellows of Harvard College.
+ * The President and Fellows of Harvard College.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -49,9 +49,9 @@
 #include <vnode.h>
 #include <vfs.h>
 #include <synch.h>
-#include <kern/fcntl.h>  
+#include <kern/fcntl.h>
 #if OPT_A2
-#include <limits.h>  
+#include <limits.h>
 #endif
 
 /*
@@ -66,10 +66,10 @@ struct proc *kproc;
 /* count of the number of processes, excluding kproc */
 static volatile unsigned int proc_count;
 /* provides mutual exclusion for proc_count */
-/* it would be better to use a lock here, but we use a semaphore because locks are not implemented in the base kernel */ 
+/* it would be better to use a lock here, but we use a semaphore because locks are not implemented in the base kernel */
 static struct semaphore *proc_count_mutex;
 /* used to signal the kernel menu thread when there are no processes */
-struct semaphore *no_proc_sem;   
+struct semaphore *no_proc_sem;
 #endif  // UW
 
 
@@ -77,26 +77,26 @@ struct semaphore *no_proc_sem;
 static volatile pid_t pid_count = PID_MIN;
 
 void save_process_status(struct proc* new, struct proc* parent) {
-  struct process_status* ps = kmalloc(sizeof(struct process_status));
-  ps->pid = new->pid;
-  ps->parent = parent ? parent->pid : 0;
-  ps->valid = true;
-  ps->process = new;
-  ps->exitcode = -1;
-  ps->cv_waitpid = cv_create(new->p_name);
-  array_add(process_table, ps, NULL);
+   struct process_status* ps = kmalloc(sizeof(struct process_status));
+   ps->pid = new->pid;
+   ps->parent = parent ? parent->pid : 0;
+   ps->valid = true;
+   ps->process = new;
+   ps->exitcode = -1;
+   ps->cv_waitpid = cv_create(new->p_name);
+   array_add(process_table, ps, NULL);
 }
 
 struct process_status* get_process_status(pid_t pid) {
-  unsigned len = array_num(process_table);
-  for (unsigned i = 0; i < len; i++) {
-    struct process_status* ps = array_get(process_table, i); 
-    // wake parents
-    if (ps && ps->pid == pid) {
-      return ps;
-    }
-  }
-  return NULL;
+   unsigned len = array_num(process_table);
+   for (unsigned i = 0; i < len; i++) {
+      struct process_status* ps = array_get(process_table, i);
+      // wake parents
+      if (ps && ps->pid == pid) {
+         return ps;
+      }
+   }
+   return NULL;
 }
 
 void process_status_destroy(struct process_status* ps) {
@@ -115,32 +115,32 @@ static
 struct proc *
 proc_create(const char *name)
 {
-	struct proc *proc;
+   struct proc *proc;
 
-	proc = kmalloc(sizeof(*proc));
-	if (proc == NULL) {
-		return NULL;
-	}
-	proc->p_name = kstrdup(name);
-	if (proc->p_name == NULL) {
-		kfree(proc);
-		return NULL;
-	}
+   proc = kmalloc(sizeof(*proc));
+   if (proc == NULL) {
+      return NULL;
+   }
+   proc->p_name = kstrdup(name);
+   if (proc->p_name == NULL) {
+      kfree(proc);
+      return NULL;
+   }
 
-	threadarray_init(&proc->p_threads);
-	spinlock_init(&proc->p_lock);
+   threadarray_init(&proc->p_threads);
+   spinlock_init(&proc->p_lock);
 
-	/* VM fields */
-	proc->p_addrspace = NULL;
+   /* VM fields */
+   proc->p_addrspace = NULL;
 
-	/* VFS fields */
-	proc->p_cwd = NULL;
+   /* VFS fields */
+   proc->p_cwd = NULL;
 
 #ifdef UW
-	proc->console = NULL;
+   proc->console = NULL;
 #endif // UW
 
-	return proc;
+   return proc;
 }
 
 /*
@@ -149,95 +149,95 @@ proc_create(const char *name)
 void
 proc_destroy(struct proc *proc)
 {
-	/*
+   /*
          * note: some parts of the process structure, such as the address space,
          *  are destroyed in sys_exit, before we get here
          *
          * note: depending on where this function is called from, curproc may not
          * be defined because the calling thread may have already detached itself
          * from the process.
-	 */
+    */
 
-	KASSERT(proc != NULL);
-	KASSERT(proc != kproc);
-	
+   KASSERT(proc != NULL);
+   KASSERT(proc != kproc);
+
 #if OPT_A2
    // if was last elem, destroy table
    P(proc_count_mutex);
    if (proc_count == 0) {
-   	for (unsigned i = 0; i < array_num(process_table); i++) {
-   		struct process_status* ps = array_get(process_table, i);
-   		KASSERT(ps);
-   		KASSERT(ps->pid == proc->pid || ps->parent == proc->pid);
-   		process_status_destroy(ps);
-   		array_remove(process_table, i);
-   	}
-   	array_destroy(process_table);
-   	lock_destroy(lk_process_table);
-   	process_table = NULL;
-   	lk_process_table = NULL;
+      for (unsigned i = 0; i < array_num(process_table); i++) {
+         struct process_status* ps = array_get(process_table, i);
+         KASSERT(ps);
+         KASSERT(ps->pid == proc->pid || ps->parent == proc->pid);
+         process_status_destroy(ps);
+         array_remove(process_table, i);
+      }
+      array_destroy(process_table);
+      lock_destroy(lk_process_table);
+      process_table = NULL;
+      lk_process_table = NULL;
    }
    V(proc_count_mutex);
 #endif
-	
-	/*
-	 * We don't take p_lock in here because we must have the only
-	 * reference to this structure. (Otherwise it would be
-	 * incorrect to destroy it.)
-	 */
 
-	/* VFS fields */
-	if (proc->p_cwd) {
-		VOP_DECREF(proc->p_cwd);
-		proc->p_cwd = NULL;
-	}
+   /*
+    * We don't take p_lock in here because we must have the only
+    * reference to this structure. (Otherwise it would be
+    * incorrect to destroy it.)
+    */
+
+   /* VFS fields */
+   if (proc->p_cwd) {
+      VOP_DECREF(proc->p_cwd);
+      proc->p_cwd = NULL;
+   }
 
 
 #ifndef UW  // in the UW version, space destruction occurs in sys_exit, not here
-	if (proc->p_addrspace) {
-		/*
-		 * In case p is the currently running process (which
-		 * it might be in some circumstances, or if this code
-		 * gets moved into exit as suggested above), clear
-		 * p_addrspace before calling as_destroy. Otherwise if
-		 * as_destroy sleeps (which is quite possible) when we
-		 * come back we'll be calling as_activate on a
-		 * half-destroyed address space. This tends to be
-		 * messily fatal.
-		 */
-		struct addrspace *as;
+   if (proc->p_addrspace) {
+      /*
+       * In case p is the currently running process (which
+       * it might be in some circumstances, or if this code
+       * gets moved into exit as suggested above), clear
+       * p_addrspace before calling as_destroy. Otherwise if
+       * as_destroy sleeps (which is quite possible) when we
+       * come back we'll be calling as_activate on a
+       * half-destroyed address space. This tends to be
+       * messily fatal.
+       */
+      struct addrspace *as;
 
-		as_deactivate();
-		as = curproc_setas(NULL);
-		as_destroy(as);
-	}
+      as_deactivate();
+      as = curproc_setas(NULL);
+      as_destroy(as);
+   }
 #endif // UW
 
 #ifdef UW
-	if (proc->console) {
-	  vfs_close(proc->console);
-	}
+   if (proc->console) {
+      vfs_close(proc->console);
+   }
 #endif // UW
 
-	threadarray_cleanup(&proc->p_threads);
-	spinlock_cleanup(&proc->p_lock);
+   threadarray_cleanup(&proc->p_threads);
+   spinlock_cleanup(&proc->p_lock);
 
-	kfree(proc->p_name);
-	kfree(proc);
+   kfree(proc->p_name);
+   kfree(proc);
 
 #ifdef UW
-	/* decrement the process count */
-        /* note: kproc is not included in the process count, but proc_destroy
-	   is never called on kproc (see KASSERT above), so we're OK to decrement
-	   the proc_count unconditionally here */
-	P(proc_count_mutex); 
-	KASSERT(proc_count > 0);
-	proc_count--;
-	/* signal the kernel menu thread if the process count has reached zero */
-	if (proc_count == 0) {
-	  V(no_proc_sem);
-	}
-	V(proc_count_mutex);
+   /* decrement the process count */
+   /* note: kproc is not included in the process count, but proc_destroy
+   is never called on kproc (see KASSERT above), so we're OK to decrement
+   the proc_count unconditionally here */
+   P(proc_count_mutex);
+   KASSERT(proc_count > 0);
+   proc_count--;
+   /* signal the kernel menu thread if the process count has reached zero */
+   if (proc_count == 0) {
+      V(no_proc_sem);
+   }
+   V(proc_count_mutex);
 #endif // UW
 }
 
@@ -247,20 +247,20 @@ proc_destroy(struct proc *proc)
 void
 proc_bootstrap(void)
 {
-  kproc = proc_create("[kernel]");
-  if (kproc == NULL) {
-    panic("proc_create for kproc failed\n");
-  }
+   kproc = proc_create("[kernel]");
+   if (kproc == NULL) {
+      panic("proc_create for kproc failed\n");
+   }
 #ifdef UW
-  proc_count = 0;
-  proc_count_mutex = sem_create("proc_count_mutex",1);
-  if (proc_count_mutex == NULL) {
-    panic("could not create proc_count_mutex semaphore\n");
-  }
-  no_proc_sem = sem_create("no_proc_sem",0);
-  if (no_proc_sem == NULL) {
-    panic("could not create no_proc_sem semaphore\n");
-  }
+   proc_count = 0;
+   proc_count_mutex = sem_create("proc_count_mutex", 1);
+   if (proc_count_mutex == NULL) {
+      panic("could not create proc_count_mutex semaphore\n");
+   }
+   no_proc_sem = sem_create("no_proc_sem", 0);
+   if (no_proc_sem == NULL) {
+      panic("could not create no_proc_sem semaphore\n");
+   }
 #endif // UW 
 }
 
@@ -273,51 +273,51 @@ proc_bootstrap(void)
 struct proc *
 proc_create_runprogram(const char *name)
 {
-	struct proc *proc;
-	char *console_path;
+   struct proc *proc;
+   char *console_path;
 
-	proc = proc_create(name);
-	if (proc == NULL) {
-		return NULL;
-	}
+   proc = proc_create(name);
+   if (proc == NULL) {
+      return NULL;
+   }
 
 #ifdef UW
-	/* open the console - this should always succeed */
-	console_path = kstrdup("con:");
-	if (console_path == NULL) {
-	  panic("unable to copy console path name during process creation\n");
-	}
-	if (vfs_open(console_path,O_WRONLY,0,&(proc->console))) {
-	  panic("unable to open the console during process creation\n");
-	}
-	kfree(console_path);
+   /* open the console - this should always succeed */
+   console_path = kstrdup("con:");
+   if (console_path == NULL) {
+      panic("unable to copy console path name during process creation\n");
+   }
+   if (vfs_open(console_path, O_WRONLY, 0, &(proc->console))) {
+      panic("unable to open the console during process creation\n");
+   }
+   kfree(console_path);
 #endif // UW
-	  
-	/* VM fields */
 
-	proc->p_addrspace = NULL;
+   /* VM fields */
 
-	/* VFS fields */
+   proc->p_addrspace = NULL;
+
+   /* VFS fields */
 
 #ifdef UW
-	/* we do not need to acquire the p_lock here, the running thread should
+   /* we do not need to acquire the p_lock here, the running thread should
            have the only reference to this process */
-        /* also, acquiring the p_lock is problematic because VOP_INCREF may block */
-	if (curproc->p_cwd != NULL) {
-		VOP_INCREF(curproc->p_cwd);
-		proc->p_cwd = curproc->p_cwd;
-	}
+   /* also, acquiring the p_lock is problematic because VOP_INCREF may block */
+   if (curproc->p_cwd != NULL) {
+      VOP_INCREF(curproc->p_cwd);
+      proc->p_cwd = curproc->p_cwd;
+   }
 #else // UW
-	spinlock_acquire(&curproc->p_lock);
-	if (curproc->p_cwd != NULL) {
-		VOP_INCREF(curproc->p_cwd);
-		proc->p_cwd = curproc->p_cwd;
-	}
-	spinlock_release(&curproc->p_lock);
+   spinlock_acquire(&curproc->p_lock);
+   if (curproc->p_cwd != NULL) {
+      VOP_INCREF(curproc->p_cwd);
+      proc->p_cwd = curproc->p_cwd;
+   }
+   spinlock_release(&curproc->p_lock);
 #endif // UW
 
 #if OPT_A2
-   P(proc_count_mutex); 
+   P(proc_count_mutex);
    spinlock_acquire(&curproc->p_lock);
    // assign pid for the first program
    KASSERT(proc);
@@ -325,26 +325,26 @@ proc_create_runprogram(const char *name)
    KASSERT(proc->pid > 0);
    // create process_table
    if (proc_count == 0) {
-     process_table = array_create();
-     lk_process_table = lock_create(curproc->p_name);
-     save_process_status(proc, 0);
-     KASSERT(proc->pid == 2);
+      process_table = array_create();
+      lk_process_table = lock_create(curproc->p_name);
+      save_process_status(proc, 0);
+      KASSERT(proc->pid == 2);
    }
    pid_count++;
    spinlock_release(&curproc->p_lock);
    V(proc_count_mutex);
 #endif
-   
+
 #ifdef UW
    /* increment the count of processes */
-        /* we are assuming that all procs, including those created by fork(),
-           are created using a call to proc_create_runprogram  */
-   P(proc_count_mutex); 
+   /* we are assuming that all procs, including those created by fork(),
+      are created using a call to proc_create_runprogram  */
+   P(proc_count_mutex);
    proc_count++;
    V(proc_count_mutex);
 #endif // UW
 
-	return proc;
+   return proc;
 }
 
 /*
@@ -354,18 +354,18 @@ proc_create_runprogram(const char *name)
 int
 proc_addthread(struct proc *proc, struct thread *t)
 {
-	int result;
+   int result;
 
-	KASSERT(t->t_proc == NULL);
+   KASSERT(t->t_proc == NULL);
 
-	spinlock_acquire(&proc->p_lock);
-	result = threadarray_add(&proc->p_threads, t, NULL);
-	spinlock_release(&proc->p_lock);
-	if (result) {
-		return result;
-	}
-	t->t_proc = proc;
-	return 0;
+   spinlock_acquire(&proc->p_lock);
+   result = threadarray_add(&proc->p_threads, t, NULL);
+   spinlock_release(&proc->p_lock);
+   if (result) {
+      return result;
+   }
+   t->t_proc = proc;
+   return 0;
 }
 
 /*
@@ -375,26 +375,26 @@ proc_addthread(struct proc *proc, struct thread *t)
 void
 proc_remthread(struct thread *t)
 {
-	struct proc *proc;
-	unsigned i, num;
+   struct proc *proc;
+   unsigned i, num;
 
-	proc = t->t_proc;
-	KASSERT(proc != NULL);
+   proc = t->t_proc;
+   KASSERT(proc != NULL);
 
-	spinlock_acquire(&proc->p_lock);
-	/* ugh: find the thread in the array */
-	num = threadarray_num(&proc->p_threads);
-	for (i=0; i<num; i++) {
-		if (threadarray_get(&proc->p_threads, i) == t) {
-			threadarray_remove(&proc->p_threads, i);
-			spinlock_release(&proc->p_lock);
-			t->t_proc = NULL;
-			return;
-		}
-	}
-	/* Did not find it. */
-	spinlock_release(&proc->p_lock);
-	panic("Thread (%p) has escaped from its process (%p)\n", t, proc);
+   spinlock_acquire(&proc->p_lock);
+   /* ugh: find the thread in the array */
+   num = threadarray_num(&proc->p_threads);
+   for (i = 0; i < num; i++) {
+      if (threadarray_get(&proc->p_threads, i) == t) {
+         threadarray_remove(&proc->p_threads, i);
+         spinlock_release(&proc->p_lock);
+         t->t_proc = NULL;
+         return;
+      }
+   }
+   /* Did not find it. */
+   spinlock_release(&proc->p_lock);
+   panic("Thread (%p) has escaped from its process (%p)\n", t, proc);
 }
 
 /*
@@ -405,20 +405,20 @@ proc_remthread(struct thread *t)
 struct addrspace *
 curproc_getas(void)
 {
-	struct addrspace *as;
+   struct addrspace *as;
 #ifdef UW
-        /* Until user processes are created, threads used in testing 
-         * (i.e., kernel threads) have no process or address space.
-         */
-	if (curproc == NULL) {
-		return NULL;
-	}
+   /* Until user processes are created, threads used in testing
+    * (i.e., kernel threads) have no process or address space.
+    */
+   if (curproc == NULL) {
+      return NULL;
+   }
 #endif
 
-	spinlock_acquire(&curproc->p_lock);
-	as = curproc->p_addrspace;
-	spinlock_release(&curproc->p_lock);
-	return as;
+   spinlock_acquire(&curproc->p_lock);
+   as = curproc->p_addrspace;
+   spinlock_release(&curproc->p_lock);
+   return as;
 }
 
 /*
@@ -428,12 +428,12 @@ curproc_getas(void)
 struct addrspace *
 curproc_setas(struct addrspace *newas)
 {
-	struct addrspace *oldas;
-	struct proc *proc = curproc;
+   struct addrspace *oldas;
+   struct proc *proc = curproc;
 
-	spinlock_acquire(&proc->p_lock);
-	oldas = proc->p_addrspace;
-	proc->p_addrspace = newas;
-	spinlock_release(&proc->p_lock);
-	return oldas;
+   spinlock_acquire(&proc->p_lock);
+   oldas = proc->p_addrspace;
+   proc->p_addrspace = newas;
+   spinlock_release(&proc->p_lock);
+   return oldas;
 }
--- os161-1.99/kern/syscall/proc_syscalls.c
+++ os161-1.99/kern/syscall/proc_syscalls.c
@@ -16,95 +16,95 @@
 /* process related stuff */
 
 static void debug() {
-  KASSERT(lk_process_table);
-  KASSERT(process_table);
-  kprintf("newtable: %d", array_num(process_table));
-  for (unsigned i = 0; i < array_num(process_table); i++) {
-    struct process_status* a = array_get(process_table, i);
-    struct process_status* p = get_process_status(a->parent);
-    kprintf("(%d,%d) ", a->pid, a->parent);
-    KASSERT(!p || a->parent == p->pid);
-  }
-  kprintf("\n");
+   KASSERT(lk_process_table);
+   KASSERT(process_table);
+   kprintf("newtable: %d", array_num(process_table));
+   for (unsigned i = 0; i < array_num(process_table); i++) {
+      struct process_status* a = array_get(process_table, i);
+      struct process_status* p = get_process_status(a->parent);
+      kprintf("(%d,%d) ", a->pid, a->parent);
+      KASSERT(!p || a->parent == p->pid);
+   }
+   kprintf("\n");
 }
 
 static void debug_status(struct process_status* ps) {
-  kprintf("process_status: pid %d, parent %d, valid %d, exitcode %d\n", ps->pid, ps->parent, ps->valid, ps->exitcode);
+   kprintf("process_status: pid %d, parent %d, valid %d, exitcode %d\n", ps->pid, ps->parent, ps->valid, ps->exitcode);
 }
 
 static void die() {
-  panic("die\n");
+   panic("die\n");
 }
 
 #endif
-  /* this implementation of sys__exit does not do anything with the exit code */
-  /* this needs to be fixed to get exit() and waitpid() working properly */
+/* this implementation of sys__exit does not do anything with the exit code */
+/* this needs to be fixed to get exit() and waitpid() working properly */
 
 void sys__exit(int exitcode) {
 
-  struct addrspace *as;
-  struct proc *p = curproc;
-  /* for now, just include this to keep the compiler from complaining about
-     an unused variable */
+   struct addrspace *as;
+   struct proc *p = curproc;
+   /* for now, just include this to keep the compiler from complaining about
+      an unused variable */
 #if OPT_A2
-  KASSERT(lk_process_table);
-  KASSERT(process_table);
-  lock_acquire(lk_process_table);
-  // remove process status
-  for (unsigned i = 0; i < array_num(process_table); i++) {
-    struct process_status* ps = array_get(process_table, i); 
-    KASSERT(ps);
-    // set self
-    if (ps->pid == curproc->pid) {
-      KASSERT(exitcode >= 0);
-      ps->exitcode = _MKWAIT_EXIT(exitcode);
-      ps->valid = false;
-      ps->process = NULL;
-      // wake parents
-      cv_broadcast(ps->cv_waitpid, lk_process_table);
-    }
-    // remove all children
-    else if (ps->parent == curproc->pid) {
-      if (ps->valid) {
-        kprintf("%d exits, its child %d is still valid\n", curproc->pid, ps->pid);
-        debug_status(get_process_status(ps->pid));
-        die();
-      };
-      process_status_destroy(ps);
-      array_remove(process_table, i);
-      i--;
-    }
-  }
-  lock_release(lk_process_table);
+   KASSERT(lk_process_table);
+   KASSERT(process_table);
+   lock_acquire(lk_process_table);
+   // remove process status
+   for (unsigned i = 0; i < array_num(process_table); i++) {
+      struct process_status* ps = array_get(process_table, i);
+      KASSERT(ps);
+      // set self
+      if (ps->pid == curproc->pid) {
+         KASSERT(exitcode >= 0);
+         ps->exitcode = _MKWAIT_EXIT(exitcode);
+         ps->valid = false;
+         ps->process = NULL;
+         // wake parents
+         cv_broadcast(ps->cv_waitpid, lk_process_table);
+      }
+      // remove all children
+      else if (ps->parent == curproc->pid) {
+         if (ps->valid) {
+            kprintf("%d exits, its child %d is still valid\n", curproc->pid, ps->pid);
+            debug_status(get_process_status(ps->pid));
+            die();
+         };
+         process_status_destroy(ps);
+         array_remove(process_table, i);
+         i--;
+      }
+   }
+   lock_release(lk_process_table);
 #else
-  (void)exitcode;
+   (void)exitcode;
 #endif
 
-  DEBUG(DB_SYSCALL,"Syscall: _exit(%d)\n",exitcode);
+   DEBUG(DB_SYSCALL, "Syscall: _exit(%d)\n", exitcode);
 
-  KASSERT(curproc->p_addrspace != NULL);
-  as_deactivate();
-  /*
-   * clear p_addrspace before calling as_destroy. Otherwise if
-   * as_destroy sleeps (which is quite possible) when we
-   * come back we'll be calling as_activate on a
-   * half-destroyed address space. This tends to be
-   * messily fatal.
-   */
-  as = curproc_setas(NULL);
-  as_destroy(as);
+   KASSERT(curproc->p_addrspace != NULL);
+   as_deactivate();
+   /*
+    * clear p_addrspace before calling as_destroy. Otherwise if
+    * as_destroy sleeps (which is quite possible) when we
+    * come back we'll be calling as_activate on a
+    * half-destroyed address space. This tends to be
+    * messily fatal.
+    */
+   as = curproc_setas(NULL);
+   as_destroy(as);
 
-  /* detach this thread from its process */
-  /* note: curproc cannot be used after this call */
-  proc_remthread(curthread);
+   /* detach this thread from its process */
+   /* note: curproc cannot be used after this call */
+   proc_remthread(curthread);
 
-  /* if this is the last user process in the system, proc_destroy()
-     will wake up the kernel menu thread */
-  proc_destroy(p);
-  
-  thread_exit();
-  /* thread_exit() does not return, so we should never get here */
-  panic("return from thread_exit in sys_exit\n");
+   /* if this is the last user process in the system, proc_destroy()
+      will wake up the kernel menu thread */
+   proc_destroy(p);
+
+   thread_exit();
+   /* thread_exit() does not return, so we should never get here */
+   panic("return from thread_exit in sys_exit\n");
 }
 
 
@@ -113,13 +113,13 @@ int
 sys_getpid(pid_t *retval)
 {
 #if OPT_A2
-  *retval = curproc->pid;
-  return 0;
+   *retval = curproc->pid;
+   return 0;
 #else
-  /* for now, this is just a stub that always returns a PID of 1 */
-  /* you need to fix this to make it work properly */
-  *retval = 1;
-  return(0);
+   /* for now, this is just a stub that always returns a PID of 1 */
+   /* you need to fix this to make it work properly */
+   *retval = 1;
+   return (0);
 #endif
 }
 
@@ -127,74 +127,74 @@ sys_getpid(pid_t *retval)
 
 int
 sys_waitpid(pid_t pid,
-      userptr_t status,
-      int options,
-      pid_t *retval)
+            userptr_t status,
+            int options,
+            pid_t *retval)
 {
 #if OPT_A2
-  int err = 0;
-  (void)options;
-  KASSERT(lk_process_table);
-  KASSERT(process_table);
-  lock_acquire(lk_process_table);
-  struct process_status* st = get_process_status(pid);
-  // process exists
-  if (st) {
-    // if process is a child
-    KASSERT(st->parent);
-    KASSERT(curproc->pid);
-    if (st->parent == curproc->pid) {
-      // if process is running
-      if (st->valid) {
-        // kprintf("%d asleep\n", curproc->pid);
-        cv_wait(st->cv_waitpid, lk_process_table);
-        // kprintf("%d woke\n", curproc->pid);
+   int err = 0;
+   (void)options;
+   KASSERT(lk_process_table);
+   KASSERT(process_table);
+   lock_acquire(lk_process_table);
+   struct process_status* st = get_process_status(pid);
+   // process exists
+   if (st) {
+      // if process is a child
+      KASSERT(st->parent);
+      KASSERT(curproc->pid);
+      if (st->parent == curproc->pid) {
+         // if process is running
+         if (st->valid) {
+            // kprintf("%d asleep\n", curproc->pid);
+            cv_wait(st->cv_waitpid, lk_process_table);
+            // kprintf("%d woke\n", curproc->pid);
+         }
+         // child exits
+         if (WEXITSTATUS(st->exitcode) < 0) {
+            kprintf("%d exits with %d\n", st->pid, st->exitcode);
+            debug_status(get_process_status(st->pid));
+            die();
+         };
+         err = copyout(&(st->exitcode), status, sizeof(int));
+         if (err) {return err;}
+         *retval = st->pid;
+         lock_release(lk_process_table);
+         return 0;
       }
-      // child exits
-      if (WEXITSTATUS(st->exitcode) < 0) {
-        kprintf("%d exits with %d\n", st->pid, st->exitcode);
-        debug_status(get_process_status(st->pid));
-        die();
-      };
-      err = copyout(&(st->exitcode),status,sizeof(int));
-      if (err) {return err;}
-      *retval = st->pid;
+      // debug();
+      // if process isn't a child
       lock_release(lk_process_table);
-      return 0;
-    }
-    // debug();
-    // if process isn't a child
-    lock_release(lk_process_table);
-    return -1;
-  }
-  // process doesn't exist
-  lock_release(lk_process_table);
-  return -1;    
+      return -1;
+   }
+   // process doesn't exist
+   lock_release(lk_process_table);
+   return -1;
 #else
-  int exitstatus;
-  int result;
+   int exitstatus;
+   int result;
 
-  /* this is just a stub implementation that always reports an
-     exit status of 0, regardless of the actual exit status of
-     the specified process.   
-     In fact, this will return 0 even if the specified process
-     is still running, and even if it never existed in the first place.
+   /* this is just a stub implementation that always reports an
+      exit status of 0, regardless of the actual exit status of
+      the specified process.
+      In fact, this will return 0 even if the specified process
+      is still running, and even if it never existed in the first place.
 
-     Fix this!
-  */
+      Fix this!
+   */
 
-  if (options != 0) {
-    return(EINVAL);
-  }
-  /* for now, just pretend the exitstatus is 0 */
-  exitstatus = 0;
-  result = copyout((void *)&exitstatus,status,sizeof(int));
-  if (result) {
-    return(result);
-  }
-  *retval = pid;
-  
-  return(0);
+   if (options != 0) {
+      return (EINVAL);
+   }
+   /* for now, just pretend the exitstatus is 0 */
+   exitstatus = 0;
+   result = copyout((void *)&exitstatus, status, sizeof(int));
+   if (result) {
+      return (result);
+   }
+   *retval = pid;
+
+   return (0);
 #endif
 }
 
@@ -203,35 +203,35 @@ sys_waitpid(pid_t pid,
 /* Fork related stuff */
 
 int sys_fork(struct trapframe* tf, pid_t* rv) {
-  KASSERT(lk_process_table);
-  KASSERT(process_table);
-  struct proc* proc = proc_create_runprogram(curproc->p_name);
-  if (! proc) {
-    return -1;
-  }
-  spinlock_acquire(&curproc->p_lock);
-  if (as_copy(curproc->p_addrspace, &proc->p_addrspace)) {
-    proc_destroy(proc);
-    spinlock_release(&curproc->p_lock);
-    return -1;
-  }
-  if (thread_fork(proc->p_name, proc, &enter_forked_process, tf, 1)) {
-    proc_destroy(proc);
-    spinlock_release(&curproc->p_lock);
-    return -1; 
-  };
-  *rv = proc->pid;
-  spinlock_release(&curproc->p_lock);
-  KASSERT(proc->pid > 0);
-  lock_acquire(lk_process_table);
-  if (proc->pid < 0) {
-    kprintf("proc->pid should be %d but got %d", *rv, proc->pid);
-    debug();
-    panic("die");
-  }
-  save_process_status(proc, curproc);
-  lock_release(lk_process_table);
-  return 0;
+   KASSERT(lk_process_table);
+   KASSERT(process_table);
+   struct proc* proc = proc_create_runprogram(curproc->p_name);
+   if (! proc) {
+      return -1;
+   }
+   spinlock_acquire(&curproc->p_lock);
+   if (as_copy(curproc->p_addrspace, &proc->p_addrspace)) {
+      proc_destroy(proc);
+      spinlock_release(&curproc->p_lock);
+      return -1;
+   }
+   if (thread_fork(proc->p_name, proc, &enter_forked_process, tf, 1)) {
+      proc_destroy(proc);
+      spinlock_release(&curproc->p_lock);
+      return -1;
+   };
+   *rv = proc->pid;
+   spinlock_release(&curproc->p_lock);
+   KASSERT(proc->pid > 0);
+   lock_acquire(lk_process_table);
+   if (proc->pid < 0) {
+      kprintf("proc->pid should be %d but got %d", *rv, proc->pid);
+      debug();
+      panic("die");
+   }
+   save_process_status(proc, curproc);
+   lock_release(lk_process_table);
+   return 0;
 }
 
 #endif
